0c475c513c8c6f9a31aa46f2568bea58dc92b02e
razidev <razisyahputro@gmail.com>
2022-02-10 12:58:16 +0700

    implementing signup & login

---
+++ 22-user-authentication/routes/demo.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bcrypt = require("bcryptjs")
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("welcome");
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const email = userData.email;
+  const confirmEmail = userData["confirm-email"];
+  const password = userData.password;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = {
+    email,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const email = userData.email;
+  const password = userData.password;
+
+  const existingUser = await db.getDb().collection("users").findOne({ email });
+
+  if (!existingUser) {
+    console.log('Could not log in!');
+    return res.redirect('/login');
+  }
+
+  const passwordEqual = await bcrypt.compare(password, existingUser.password);
+  if (!passwordEqual) {
+    console.log('Could not log in! - password arent equal');
+    return res.redirect('/login');
+  }
+
+  console.log('User is authenticated');
+  res.redirect('/admin');
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
+
+module.exports = router;
