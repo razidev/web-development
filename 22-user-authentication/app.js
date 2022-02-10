18474348567e97854a654756e99ae404d6a36383
razidev <razisyahputro@gmail.com>
2022-02-10 14:45:35 +0700

    add session

--- 22-user-authentication/app.js
+++ 22-user-authentication/app.js
@@ -1,17 +1,34 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: 'mongodb://localhost:27017',
+  databaseName: 'auth-demo',
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: "your-secret-code",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
 
 app.use(demoRoutes);
 
