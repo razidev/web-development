228280be12f9b333a370ff6c0248bf1f6b9fbe74
razidev <razisyahputro@gmail.com>
2022-02-11 20:49:52 +0700

    storing data into session

--- 22-user-authentication/routes/demo.js
+++ 22-user-authentication/routes/demo.js
@@ -69,8 +69,11 @@ router.post("/login", async function (req, res) {
     return res.redirect("/login");
   }
 
-  console.log("User is authenticated");
-  res.redirect("/admin");
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function() {
+    res.redirect("/admin");
+  });
 });
 
 router.get("/admin", function (req, res) {
