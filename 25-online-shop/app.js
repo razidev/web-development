4bfb2e41ed4b2ae2563e0733322a7e1edd95f125
razidev <razisyahputro@gmail.com>
2022-02-20 15:58:27 +0700

    implement ejs engine

--- 25-online-shop/app.js
+++ 25-online-shop/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const authRoutes = require('./routes/auth.routes');
+
+app.set('view engine', 'ejs');//to activate rendering views using ejs
+app.set('views', path.join(__dirname, 'views'));//to set the path to the views folder
 
 app.use(authRoutes);
 
