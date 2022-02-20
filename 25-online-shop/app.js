f2b7766c4f450b077de2339352e5173bc4f94969
razidev <razisyahputro@gmail.com>
2022-02-20 16:17:12 +0700

    implement font & use static folder on public folder

--- 25-online-shop/app.js
+++ 25-online-shop/app.js
@@ -7,6 +7,8 @@ const authRoutes = require('./routes/auth.routes');
 app.set('view engine', 'ejs');//to activate rendering views using ejs
 app.set('views', path.join(__dirname, 'views'));//to set the path to the views folder
 
+app.use(express.static('public'));
+
 app.use(authRoutes);
 
 app.listen(3000);
