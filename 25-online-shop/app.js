76b04dc8191bd5a9f02d10e7999b43e400255071
razidev <razisyahputro@gmail.com>
2022-03-07 08:06:59 +0700

    add csrf from csurf

--- 25-online-shop/app.js
+++ 25-online-shop/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const csrf = require('csurf');
 const app = express();
 const db = require('./data/database');
+const csrfTokenMiddleware = require('./middlewares/csrf-token');
 const authRoutes = require('./routes/auth.routes');
 
 app.set('view engine', 'ejs');//to activate rendering views using ejs
@@ -9,6 +11,9 @@ app.set('views', path.join(__dirname, 'views'));//to set the path to the views f
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(csrf());
+app.use(csrfTokenMiddleware);
 
 app.use(authRoutes);
 
