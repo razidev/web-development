a72ee04b4f84a7ea58337fc724d18aea8a8cdea4
razidev <razisyahputro@gmail.com>
2022-03-09 14:36:00 +0700

    handling error async code

--- 25-online-shop/controllers/auth.controller.js
+++ 25-online-shop/controllers/auth.controller.js
@@ -5,7 +5,7 @@ function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,7 +14,13 @@ async function signup(req, res) {
     req.body.postal,
     req.body.city
   );
-  await user.signup();
+
+  try {
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
   res.redirect('/login');
 }
 
@@ -24,7 +30,13 @@ function getLogin(req, res) {
 
 async function login(req, res) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserByEmail();
+  let existingUser;
+  try {
+    existingUser = await user.getUserByEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   if (!existingUser) {
     res.redirect('/login');
