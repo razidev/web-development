5067521097e61c65ed6cd62188c14474a81a5fab
razisyahputro <razi.syahputro@modalrakyat.id>
2022-03-21 21:39:54 +0700

    managing orders as an administrator

--- 25-online-shop/routes/admin.routes.js
+++ 25-online-shop/routes/admin.routes.js
@@ -9,5 +9,7 @@ router.post('/products', imageUploadMiddleware, adminController.createNewProduct
 router.get('/products/:id', adminController.getUpdateProduct);
 router.post('/products/:id', imageUploadMiddleware, adminController.updateProduct);
 router.delete('/products/:id', adminController.deleteProduct);
+router.get('/orders', adminController.getOrders);
+router.patch('/orders/:id', adminController.updateOrder);
 
 module.exports = router;
