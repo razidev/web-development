56aac6fdcfdaa5f09151dc6d4b909d4076b63ded
razidev <razisyahputro@gmail.com>
2022-04-03 13:24:43 +0700

    patch & delete request

--- 29-vue-basic/scripts/app.js
+++ 29-vue-basic/scripts/app.js
@@ -20,6 +20,29 @@ const TodosApp = {
                 };
                 this.todos[todoIndex] = updateTodoItem;
                 this.editedTodoId = null;
+
+                let response;
+                try {
+                    response = await fetch('http://localhost:3000/todos/' + todoId, {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        text: this.enteredTodoText,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    });
+                } catch (error) {
+                    alert('Something went wrong!');
+                    return;
+                }
+
+                if (!response.ok) {
+                    alert('Something went wrong!');
+                    return;
+                }
+
+                
             } else {
                 let response;
                 try {
@@ -56,8 +79,23 @@ const TodosApp = {
             const todo = this.todos.find(todo => todo.id === todoId);
             this.enteredTodoText = todo.text;
         },
-        deleteTodo(todoId) {
+        async deleteTodo(todoId) {
             this.todos = this.todos.filter(todo => todo.id !== todoId);
+            let response;
+
+            try {
+                response = await fetch('http://localhost:3000/todos/' + todoId, {
+                method: 'DELETE',
+                });
+            } catch (error) {
+                alert('Something went wrong!');
+                return;
+            }
+
+            if (!response.ok) {
+                alert('Something went wrong!');
+                return;
+            }
         }
     },
     async created() {
