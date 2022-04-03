e17759d0b8689e77a5b2c5a0c635233fc8489d40
razidev <razisyahputro@gmail.com>
2022-04-03 13:11:05 +0700

    loading data & manage loading

--- 29-vue-basic/scripts/app.js
+++ 29-vue-basic/scripts/app.js
@@ -1,6 +1,7 @@
 const TodosApp = {
     data() {
         return {
+            isLoading: false,
             todos: [],
             enteredTodoText: '',
             editedTodoId: null,
@@ -58,6 +59,27 @@ const TodosApp = {
         deleteTodo(todoId) {
             this.todos = this.todos.filter(todo => todo.id !== todoId);
         }
+    },
+    async created() {
+        let response;
+        this.isLoading = true;
+        try {
+            response = await fetch('http://localhost:3000/todos');
+        } catch (error) {
+            alert('Something went wrong!');
+            this.isLoading = false;
+            return;
+        }
+
+        this.isLoading = false;
+
+        if (!response.ok) {
+            alert('Something went wrong!');
+            return;
+        }
+
+        const responseData = await response.json();
+        this.todos = responseData.todos;
     }
 };
 
