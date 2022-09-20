bc77d162e78425e58d97ca57aa080d9f64282efe
razisyahputro <razisyahputro@gmail.com>
2022-09-20 23:49:06 +0700

    crud with mongodb

---
+++ 19-express-mongodb/routes/blog.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.redirect("/posts");
+});
+
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({})
+    .project({ title: 1, summary: 1, "author.name": 1 })
+    .toArray();
+  res.render("posts-list", { posts });
+});
+
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  res.redirect("/posts");
+});
+
+router.get("/posts/:id", async (req, res) => {
+  let postId = req.params.id;
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render("404");
+  }
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: postId });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  post.date = post.date.toISOString();
+
+  res.render("post-detail", { post });
+});
+
+router.get("/posts/:id/edit", async (req, res) => {
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(req.params.id) });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post });
+});
+
+router.post("/posts/:id/edit", async (req, res) => {
+  const query = await db.getDb().collection("posts").updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: {
+      title: req.body.title,
+      summary: req.body.summary,
+      body: req.body.content,
+    }}
+  );
+
+  res.redirect("/posts");
+});
+
+router.post('/posts/:id/delete', async (req, res) => {
+  await db.getDb().collection('posts').deleteOne({_id: new ObjectId(req.params.id)})
+  res.redirect('/posts');
+});
+module.exports = router;
